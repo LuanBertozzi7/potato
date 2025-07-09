d1a15c521e70c6c42bdf0c47565a6d51302b0a18
LuanBertozzi <LuanBertozziS2@hotmail.com>
2025-07-09 00:16:25 -0400

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 export * from "colors";
 
+import config from "./config.json";
 import { ExtendedClient } from "./structs/ExtendedClient";
+
+export { client, config };
 
 const client = new ExtendedClient(); 
 client.start();
-export { client };
 
+/* Bot initial Messages */
 client.on("ready", () => {
   console.log("Bot is online!".green);
   console.log(`Logged in as ${client.user?.tag}`.blue);
 })
-
 client.on("messageCreate", (message)=> {
-  if(message.content === "!ping") {
+  if(message.content === "ping") {
     message.reply("e é é?");
   }
+
 })
